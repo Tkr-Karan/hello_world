223ba1f053975a53db1ecbd4d6b34d61c0a54ef4
JJ-Mark <64181770+JJ-Mark@users.noreply.github.com>
2021-10-06 13:19:10 +0530

    Update lambda.py

--- lambda.py
+++ lambda.py
@@ -1,2 +1,4 @@
 # change the name of the file becasue it's a simple printing the hello world, here we don't use any type of lambda function.
+#please write the lambda function for this code.
+
 print('Hello World')
