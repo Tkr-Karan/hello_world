dda0bb0f112f87dcbedf864b9a4cb53a1b4b9b64
Karan Kumar <72157767+Tkr-Karan@users.noreply.github.com>
2021-10-06 13:25:22 +0530

    Update lambda.py

--- lambda.py
+++ lambda.py
@@ -1,4 +1,6 @@
 # change the name of the file becasue it's a simple printing the hello world, here we don't use any type of lambda function.
 #please write the lambda function for this code.
 vervve
+#i wanna add some data
+
 print('Hello World')
